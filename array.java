33ed543b48a0511bee98e804b0e78435ba83a1de
SRUTHI <128819070+sruthiveni@users.noreply.github.com>
2024-05-26 21:10:31 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,25 @@
+package mmmmm;
+import java.util.*;
+public class Arrayprime {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int flag=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=1;j<n;j++)
+			{
+				if(i%j==0)
+				{
+					
+				}
+			}
+		}
+	}
+}
